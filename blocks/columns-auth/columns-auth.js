2b943830d55e05be51c8ec17ad667475816870e2
Victor Deb <victor.deb@codeandtheory.com>
2026-08-06 20:11:21 +0530

    lint issue fixed

--- blocks/columns-auth/columns-auth.js
+++ blocks/columns-auth/columns-auth.js
@@ -75,11 +75,11 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-auth-${cols.length}-cols`);
 
-  const colHeading = block.querySelector(".columns-auth h1");
-  if (colHeading && colHeading.textContent.includes("®") && !colHeading.querySelector(".registered")) {
+  const colHeading = block.querySelector('.columns-auth h1');
+  if (colHeading && colHeading.textContent.includes('®') && !colHeading.querySelector('.registered')) {
     colHeading.innerHTML = colHeading.innerHTML.replace(
       /®/g,
-      '<sup class="registered">®</sup>'
+      '<sup class="registered">®</sup>',
     );
   }
 
